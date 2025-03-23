4a425a96c16e38e19eb0cf596f98c0ebda79b42c
zshnrg <ghosanirozan1@gmail.com>
2025-03-24 01:39:27 +0700

    test: user item unit testing

--- src/app/shared/user-item/user-item.component.spec.ts
+++ src/app/shared/user-item/user-item.component.spec.ts
@@ -1,23 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserItemComponent } from './user-item.component';
+import { User } from '../../core/types/user.type';
+import { USERS } from '../../features/user-list/user.mock';
+import { provideRouter } from '@angular/router';
+import { ButtonComponent } from '../button/button.component';
+import { By } from '@angular/platform-browser';
 
 describe('UserItemComponent', () => {
   let component: UserItemComponent;
   let fixture: ComponentFixture<UserItemComponent>;
 
+  const mockUser: User = USERS[0]
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserItemComponent]
-    })
-    .compileComponents();
+      imports: [UserItemComponent, ButtonComponent],
+      providers: [
+        provideRouter([])
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserItemComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('user', mockUser);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the user name and username', () => {
+    const nameElement = fixture.debugElement.query(By.css('h2')).nativeElement;
+    const usernameElement = fixture.debugElement.query(By.css('p')).nativeElement;
+
+    expect(nameElement.textContent).toContain(mockUser.name);
+    expect(usernameElement.textContent).toContain('@' + mockUser.username);
+  });
+
+  it('should have the correct routerLink on the main div', () => {
+    const divElement = fixture.debugElement.query(By.css('div'));
+    expect(divElement.attributes['ng-reflect-router-link']).toBe(`/users,${mockUser.id}`);
+  });
+
+  it('should have the correct routerLink on the button', () => {
+    const buttonElement = fixture.debugElement.query(By.css('app-button'));
+    expect(buttonElement.attributes['ng-reflect-router-link']).toBe(`/users,${mockUser.id}`);
+  });
+
+  it('should render the app-button component', () => {
+    const buttonElement = fixture.debugElement.query(By.css('app-button'));
+    expect(buttonElement).toBeTruthy();
   });
 });
